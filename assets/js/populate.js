2e55b87bc5553a0bd63fcf44a5473d5472808a61
Pedro Mealha <pedro.mealha@ultimate.ai>
2024-05-02 12:24:18 +0100

    fix missing product

--- assets/js/populate.js
+++ assets/js/populate.js
@@ -1,11 +1,12 @@
 export async function populateHtml(products) {
   new DataTable('#products-table', {
+    autoWidth: false,
     data: products,
     columns: [
-      { data: 'id', title: 'ID' },
-      { data: 'name', title: 'Name' },
-      { data: 'oldPrice', title: 'Old Price' },
-      { data: 'newPrice', title: 'New Price' }
+      { data: 'id', title: 'ID', width: '20%' },
+      { data: 'name', title: 'Name', width: '60%' },
+      { data: 'oldPrice', title: 'Old Price', type: 'num-fmt', width: '10%' },
+      { data: 'newPrice', title: 'New Price', type: 'num-fmt', width: '10%' }
     ]
   });
 
